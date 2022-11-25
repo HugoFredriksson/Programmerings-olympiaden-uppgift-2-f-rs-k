594455bd64df69a1d3ae2471c8f0ce4c49129fd6
HugoFredriksson <89245482+HugoFredriksson@users.noreply.github.com>
2022-11-25 10:08:54 +0100

    fungerar inte men försökte....

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string input = "CBCABDJ";
+
+string output = string.Empty;
+
+foreach(char c in input)
+{
+    if(char.IsNumber(c))
+    {
+        output += c;
+    }
+    else
+    {
+        output += ((byte)c).ToString();
+
+        Console.WriteLine(output);
+    }           
+}
